a8357c9edcaf8285006030cd1002fa9fe78fcccb
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2020-09-21 19:52:19 +0200

    Empty slate

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-          <v-text-field
-            :type="showPassword ? 'text' : 'password'"
-            label="Password"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-			@click:append="showPassword = !showPassword"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer/>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-app>
+  <!-- App.vue -->
+
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    
+  </v-footer>
+</v-app>
+
 </template>
 
 <script>
