5512af97695e1cad588fafe7d380953a52f61579
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2020-09-30 11:22:51 +0200

    header and icons added

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,8 +1,36 @@
 <template>
   <v-container>
     <v-app-bar app>
-      <!-- -->
+      <v-toolbar-title>Photo Meetups</v-toolbar-title>
+      <!-- --><v-spacer
+      ></v-spacer>
+
+      <span class="hidden-sm-and-up">
+        <v-btn>Menu</v-btn>
+      </span>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn to="/" text>
+          <v-icon small left>mdi-home</v-icon>
+          Home
+        </v-btn>
+
+        <v-btn to="SingleEvent" text>
+          <v-icon small left>mdi-domain</v-icon>
+          Event
+        </v-btn>
+        <v-btn to="ReviewPage" text>
+          <v-icon small left>mdi-pencil</v-icon>
+          Review
+        </v-btn>
+        <v-btn to="About" text>
+          <v-icon small left>mdi-book</v-icon>
+          About
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      Hi there
+    </v-navigation-drawer>
   </v-container>
 </template>
 
@@ -11,6 +39,7 @@ export default {
   name: "HeaderBar",
 
   data: () => ({
+    drawer: false,
     //
   }),
 };
--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,7 +1,26 @@
-import { mount } from "@vue/test-utils";
+import { createLocalVue, mount } from "@vue/test-utils";
 import App from "@/App.vue";
+import Vuetify from "vuetify";
 
 describe("App", () => {
+  const localVue = createLocalVue();
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+  it("should have a custom title and match snapshot", () => {
+    const wrapper = mount(App, {
+      localVue,
+      vuetify,
+      propsData: { title: "Foobar" },
+    });
+
+    const title = wrapper.find(".v-card__title > span");
+
+    expect(title.text()).toBe("Foobar");
+  });
+
   // Inspect the raw component options
   it("has data", () => {
     expect(typeof App.data).toBe("function");
@@ -9,13 +28,13 @@ describe("App", () => {
 });
 
 describe("Mounted App", () => {
-	const wrapper = mount(App)
+  const wrapper = mount(App);
 
-	test("is a Vue instance", () => {
-		expect(wrapper.isVueInstance()).toBeTruthy()
-	})
+  test("is a Vue instance", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
 
-	it("renders the correct markup", () => {
-		expect(wrapper.html()).toContain("<p>How are you?</p>")
-	})
-})
+  it("renders the correct markup", () => {
+    expect(wrapper.html()).toContain("<p>How are you?</p>");
+  });
+});
